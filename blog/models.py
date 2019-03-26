61ff7da7c5ccc388f433062e5714a4f75e2a3d11
Szymon <szymon.ludyga2@gmail.com>
2019-03-26 10:54:49 +0100

    changes to heroku

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
