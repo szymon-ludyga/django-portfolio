add58f3783fb1368a2318b70fa270fc30b3f2ec8
Szymon <szymon.ludyga2@gmail.com>
2019-03-26 11:55:57 +0100

    #1

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolio/static/')
+    os.path.join(BASE_DIR, 'portfolio/staticfiles/')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
